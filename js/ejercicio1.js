436f59672b32d3ce458e22af0b54d8efa5715261
michele <stoicom@hotmail.com>
2018-10-02 11:04:44 +0200

    ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,38 @@
+//isAdult(17)
+
+function isAdult(age){
+    // if(age>=18){
+    //     console.log("es mayor")  
+    // }
+    // else{
+    //     console.log("no es mayor")
+    // }
+    
+    console.log("Es " + (age >= 18 ? "mayor" : "menor"));
+
+  }
+
+  nacimiento()
+
+  function nacimiento(){
+
+
+    var fecha = new Date(1981, 3, 10);
+
+    
+    // console.log(fecha);
+    console.log( "Nací el día " + fecha.getDate() + " del mes " + fecha.getMonth() + " del año " + fecha.getFullYear() );
+
+
+      // if(age>=18){
+      //     console.log("es mayor")
+      // }
+      // else{
+      //     console.log("no es mayor")
+      // }
+      
+     // console.log("Es " + (age >= 18 ? "mayor" : "menor"));
+  
+    }
+
+    
